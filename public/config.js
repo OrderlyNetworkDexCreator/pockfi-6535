e853b867ede0da2c1a4987f1a383df16694c6072
Mario Reder <mario.reder@pm.me>
2025-10-17 20:55:39 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -16,7 +16,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_NAME": "pockfi",
   "VITE_APP_DESCRIPTION": "Pocket Finance, Exchange, DeFi, DEX, Trade",
   "VITE_HAS_PRIMARY_LOGO": "true",
-  "VITE_HAS_SECONDARY_LOGO": "false",
+  "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Markets,Trading,Swap,Portfolio,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "false",
