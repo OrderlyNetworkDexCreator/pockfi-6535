7691f68bfb186ac78c13998394ffb5d06d33e786
Mario Reder <mario.reder@pm.me>
2026-05-06 15:10:17 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Portfolio,Trading,Leaderboard,Rewards",
-  "VITE_CUSTOM_MENUS": "Info Hub, https://blog.pockfi.com/p/welcome-to-pocket-finance-trade-today.html; Chat, https://blog.pockfi.com/p/pockfi-community-chat.html",
+  "VITE_CUSTOM_MENUS": "Info Hub, https://blog.pockfi.com/p/welcome-to-pocket-finance-trade-today.html; Chat, https://blog.pockfi.com/p/pockfi-community-chat.html; Legacy, https://dex.orderly.network/demo/perp/PERP_ETH_USDC",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
